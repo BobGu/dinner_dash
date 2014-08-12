f2cf49af7752454a127b657d835b3f34f0c5c968
timproctor <timandcamiproctor@gmail.com>
2014-08-12 17:36:32 -0600

    Created spec order views, and for order controller**RED specs

---
+++ spec/views/orders/edit.html.erb_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "orders/edit", :type => :view do
+  before(:each) do
+    @order = assign(:order, Order.create!(
+      :user_id         => 1
+      :order_total     => 100.00
+      :order_type      => "Pickup"
+      :deliver_address => "1510 Blake Street"
+      :order_status    => "Paid"
+    ))
+  end
+
+  it "renders the edit order form" do
+    render
+
+      assert_select "form[action=?][method=?]", order_path(@order), "post" do
+
+      assert_select "input#order_user_id[user_id=?]", "order[user_id]"
+
+      assert_select "input#order_order_total[order_total=?]", "order[order_total]"
+
+      assert_select "input#order_order_type[order_type=?]", "order[order_type]"
+
+      assert_select "input#order_deliver_address[deliver_address=?]", "order[deliver_address]"
+
+      assert_select "input#order_order_status[order_status]", "order[order_status]"
+    end
+  end
+end
---
+++ spec/views/orders/index.html.erb_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "orders/index", :type => :view do
+  before(:each) do
+    assign(:orders, [
+      Order.create!(
+      :user_id         => 1
+      :order_total     => 100.00
+      :order_type      => "Pickup"
+      :deliver_address => "1510 Blake Street"
+      :order_status    => "Paid"
+      ),
+      Order.create!(
+      :user_id         => 2
+      :order_total     => 30.00
+      :order_type      => "Delivery"
+      :deliver_address => "1550 Market Street"
+      :order_status    => "Unpaid"
+      )
+    ])
+  end
+
+  it "renders a list of orders" do
+    render
+    assert_select "li", :text => "Orders".to_s, :count => 1
+  end
+end
