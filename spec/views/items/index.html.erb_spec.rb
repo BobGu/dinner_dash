5602716f4f441c7c51d0c48642954b9cb56fa9b5
Robert Gu <verygudhokie@gmail.com>
2014-08-12 11:30:56 -0600

    model tests for item created and passing, index view for items, rendering and passing

---
+++ spec/views/items/index.html.erb_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'items/index', :type => :view do
+  before(:each) do
+    assign(:items, [
+      Item.create!(
+      :title => "Baltimore Bomb",
+      :description => "Yay",
+      :price_slice => 6.50,
+      :price_pie => 30.00
+      ),
+      Item.create!(
+      :title => "Key Lime",
+      :description => "Yum",
+      :price_slice => 7.00,
+      :price_pie => 25.00
+      )
+    ])
+  end
+
+  it "renders a list of items" do
+    render
+    assert_select "li", :text => "Baltimore Bomb".to_s
+  end
+
+end
