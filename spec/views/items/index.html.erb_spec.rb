c8aa68e864712eae26b3416d488397cc3e13a621
emilyadavis303 <emilyadavis303@gmail.com>
2014-08-12 16:52:47 -0600

    sets up item view

--- spec/views/items/index.html.erb_spec.rb
+++ spec/views/items/index.html.erb_spec.rb
@@ -9,18 +9,22 @@ RSpec.describe 'items/index', :type => :view do
       :price_slice => 6.50,
       :price_pie => 30.00
       ),
-      Item.create!(
-      :title => "Key Lime",
-      :description => "Yum",
-      :price_slice => 7.00,
-      :price_pie => 25.00
-      )
+    #   Item.create!(
+    #   :title => "Key Lime",
+    #   :description => "Yum",
+    #   :price_slice => 7.00,
+    #   :price_pie => 25.00
+    #   )
     ])
   end
 
   it "renders a list of items" do
     render
-    assert_select "li", :text => "Baltimore Bomb".to_s
+    assert_select ".pie_title", :text => "Baltimore Bomb"
+    assert_select ".pie_description", :text => "Yay"
+    assert_select ".pie_price", :text => "6.50"
+    # assert_select "li", :text => "Yay"
+    # assert_select "li", :text => 7.00
   end
 
 end
