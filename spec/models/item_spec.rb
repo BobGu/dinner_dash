16fc7606cc4051a8a968cfd185591d1b1cd302fb
emilyadavis303 <emilyadavis303@gmail.com>
2014-08-12 11:46:02 -0600

    Just checking in.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Item, :type => :model do
+  
   let(:item) do
     Item.new(title: 'Baltimore Bomb', description: 'Delicious pie.', price_slice: 6.50, price_pie: 30.00)
   end
@@ -11,23 +12,26 @@ RSpec.describe Item, :type => :model do
 
   it 'is invalid without a title' do
     item.title = nil
+    
     expect(item).not_to be_valid
   end
 
   it 'is invalid without a description' do
     item.description = nil
+    
     expect(item).not_to be_valid
   end
 
   it 'is invalid without a slice price' do
     item.price_slice = nil
+    
     expect(item).not_to be_valid
   end
 
   it 'is invalid without a pie price' do
     item.price_pie = nil
+    
     expect(item).not_to be_valid
   end
-
 
 end
