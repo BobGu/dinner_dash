2fbeb87b9b7a4c345a49aed0e7dfb874330f969d
timproctor <timandcamiproctor@gmail.com>
2014-08-19 16:00:57 -0600

    GREEN for viewing orders with different statuses

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,13 @@
 class Order < ActiveRecord::Base
+  class Status
+    PAID = "paid"
+    CANCELLED = "cancelled"
+    COMPLETED = "completed"
+    ORDERED = "ordered"
+
+    ALL = [PAID, CANCELLED, COMPLETED, ORDERED]
+  end
+  
   has_many    :order_items
   has_many    :items, through: :order_items
   #belongs_to  :user
@@ -9,5 +18,6 @@ class Order < ActiveRecord::Base
   # validates :delivery_address, presence: true
   #delivery address needs to validate only when it is delivery and not pick-up
   validates :order_status,     presence: true
+  validates :order_status, inclusion: {in: Status::ALL}
 
 end
--- spec/features/order_view_spec.rb
+++ spec/features/order_view_spec.rb
@@ -4,38 +4,27 @@ require 'capybara/rspec'
 
 describe 'the order view', type: :feature do
 
-		let(:order1) do
-			Order.create(
-				user_id: 1156,
-				order_total: 135.00,
-				order_type: "Pickup",
-				delivery_address: "1510 Blake",
-				order_status: "paid"
+	context 'with orders of different statuses' do
+		before(:each) do
+
+			Order::Status::ALL.each do |status|
+				Order.create(
+					user_id: 1156,
+					order_total: 135.00,
+					order_type: "Pickup",
+					delivery_address: "1510 Blake",
+					order_status: status
 				)
 			end
-
-		let(:order2) do
-			Order.create(
-				user_id: 1156,
-				order_total: 135.00,
-				order_type: "Pickup",
-				delivery_address: "1510 Blake",
-				order_status: "ordered"
-				)
-
-		  end
-
-	describe 'order status' do
-		before(:each) do
-      order1
-			order2
 		end
 
-    it "view orders by paid status" do
-			visit orders_path
-			click_on("Paid")
-			expect(page).to have_content("paid")
-			expect(page).not_to have_content("ordered")
+		Order::Status::ALL.each do |status|
+			it "views orders by #{status} status" do
+				visit orders_path
+				click_on(status.capitalize)
+				expect(page).to have_content(status.downcase)
+				expect(page).not_to have_content(Order::Status::ALL.reject {|s| s == status })
+			end
 		end
 	end
 end
