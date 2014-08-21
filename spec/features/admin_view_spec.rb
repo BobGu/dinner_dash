5393cada55365bf6f8c06de667d615d1fdf98b84
timproctor <timandcamiproctor@gmail.com>
2014-08-21 14:41:22 -0600

    initial save

--- spec/features/admin_view_spec.rb
+++ spec/features/admin_view_spec.rb
@@ -11,18 +11,28 @@ describe 'a admin viewing the items page', type: :feature do
                 :password => 'password')
   end
 
-  it 'can login with the correct information' do
-    # visit the login page
-    page.visit login_path
-    # enter their information
-    page.fill_in('email', :with => 'john_doe@example.com')
-    page.fill_in('password', :with => 'password')
-    page.click_button('Login')
-    # then validate the email/password
+  context "admin user functionality" do
+    it 'can login with the correct information' do
+      # visit the login page
+      page.visit login_path
+      # enter their information
+      page.fill_in('email', :with => 'john_doe@example.com')
+      page.fill_in('password', :with => 'password')
+      page.click_button('Login')
+      # then validate the email/password
 
-    # see a validation that they have logged in
-    page.has_content('Thanks for loggin in admin')
-    #it should redirect them to the admin dashboard page
-    expect(current_page).to eq(admin_path)
+      # see a validation that they have logged in
+      page.has_content('Thanks for loggin in admin')
+      #it should redirect them to the admin dashboard page
+      expect(current_page).to eq(admin_path)
+    end
+
+    it 'can see a view that allows me to see item attributes on an order' do
+      # a logged in admin sees the page of orders
+      # 
+    end
+
   end
+
+
 end
