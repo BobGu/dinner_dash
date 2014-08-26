d71dc90fabe486021dd7c5f07cecc71e9e126379
emilyadavis303 <emilyadavis303@gmail.com>
2014-08-26 10:58:01 -0600

    Trying to get increase/decrease quantity in cart to work.

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -7,6 +7,20 @@ class CartItemsController < ApplicationController
     render nothing: true
   end
 
+  def increment
+    cart_item = CartItem.find(params[:id])
+    @cart     = Cart.find(params[:cart_id])
+    @cart.items << cart_item.item
+    redirect_to cart_path(@cart)
+  end
+
+  def decrement
+  	@cart     = Cart.find(params[:cart_id])
+  	cart_item = @cart.cart_items.find(params[:id])
+  	cart_item.delete if cart_item
+  	redirect_to cart_path(@cart)
+  end
+
   def destroy
     item = cart.cart_items.find_by(item_id: params[:item_id])
     cart.cart_items.destroy(item)
--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,11 @@ Rails.application.routes.draw do
       get :decrement, on: :member
     end
   end
+
   patch '/order/:id/cancel' => 'orders#cancel',   as: :cancel_order
   patch '/pay/:id/pay'      => 'orders#pay',      as: :pay_order
   patch '/pay/:id/complete' => 'orders#complete',  as: :complete_order
+
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
 
@@ -27,11 +29,16 @@ Rails.application.routes.draw do
   put     '/cart'             => 'carts#update'
   post    '/cart/items'       => 'cart_items#create',       as: :cart_items
   delete  '/cart/items'       => 'cart_items#destroy',      as: :cart_items_destroy
+  get     '/cart/items'       => 'cart_items#increment',    as: :cart_cart_items_increment
+  get     '/cart/items'       => 'cart_items#decrement',    as: :cart_cart_items_decrement
+
+  increment_order_order_item GET    /carts/:cart_id/cart_items/ cart_items#increment
+decrement_order_order_item GET    /orders/:order_id/order_items/:id/decrement(.:format) order_items#decrement
 
   get     '/order_items/new'  => 'order_items#new',         as: :new_order_item
   post    '/order_items'      => 'order_items#create',      as: :order_items
 
-  get     '/contact'          => 'contact#show'
+  get     '/contact'          => 'contact#show',            as: :contact
 
   namespace :admin do
     resources :items
