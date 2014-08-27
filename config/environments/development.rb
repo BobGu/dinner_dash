26602164fcd915d38ee640ca2cec2cb9f6e083d3
emilyadavis303 <emilyadavis303@gmail.com>
2014-08-27 15:52:48 -0600

    I can upload imagezzzzzzzzz.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,4 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   Paperclip.options[:command_path] = "/usr/local/bin/"
-  Paperclip.options[:log] = true
-  Paperclip.option[:log_command] = true
 end
