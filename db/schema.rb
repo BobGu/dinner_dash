8a55f559479f767230214a5ad14c6839fd1f115e
timproctor <timandcamiproctor@gmail.com>
2014-10-23 12:13:43 -0600

    added a readme

--- db/schema.rb
+++ db/schema.rb
@@ -44,7 +44,6 @@ ActiveRecord::Schema.define(version: 20140828003107) do
   create_table "items", force: true do |t|
     t.string   "title"
     t.string   "description"
-    t.decimal  "price_slice",          precision: 4, scale: 2
     t.decimal  "price_pie",            precision: 4, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
